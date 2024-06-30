6c13226029e922b2e7f0b3c35db2487e1a22b36e
mikeyang <18604713262@163.com>
2024-06-29 23:21:45 -0400

    Logging

--- src/main/java/pw/pdm/pdmserver/tasks/SessionCleanupTask.java
+++ src/main/java/pw/pdm/pdmserver/tasks/SessionCleanupTask.java
@@ -1,5 +1,7 @@
 package pw.pdm.pdmserver.tasks;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -7,6 +9,7 @@ import pw.pdm.pdmserver.services.SessionKeyService;
 
 @Component
 public class SessionCleanupTask {
+    private static final Logger logger = LoggerFactory.getLogger(SessionCleanupTask.class);
 
     @Autowired
     private SessionKeyService sessionKeyService;
@@ -14,6 +17,7 @@ public class SessionCleanupTask {
     @Scheduled(fixedRate = 3600000) // Run every hour
     public void cleanupExpiredSessions() {
 
+        logger.info("Running session cleanup task.");
         sessionKeyService.cleanupExpiredSessions();
     }
 }
